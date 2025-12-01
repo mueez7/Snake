a3047b0afb413a093336570138e14d8fc4ffe405
mueez7 <1975abdulmueez@gmail.com>
2025-12-01 16:58:32 +0530

    second commit

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
 
-for(let i=0; i<rows*cols; i++){
-    const block = document.createElement('div');
-    block.classList.add('block');
-    board.appendChild(block);
+// for(let i=0; i<rows*cols; i++){
+//     const block = document.createElement('div');
+//     block.classList.add('block');
+//     board.appendChild(block);
+// }
+
+for (let row=0; row<rows; row++){
+    for (let col=0; col<cols; col++){
+        const block = document.createElement('div');
+        block.classList.add('block');
+        board.appendChild(block);
+}
 }
